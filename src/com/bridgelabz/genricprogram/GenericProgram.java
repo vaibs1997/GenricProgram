df62497eb35683ea3b86fb897ad8f3a59a1cb9b7
vaibs <vaibhavmuratkar2@gmail.com>
2022-01-06 00:07:51 +0530

    Adding the generic class of any type data

--- src/com/bridgelabz/genricprogram/GenericProgram.java
+++ src/com/bridgelabz/genricprogram/GenericProgram.java
@@ -3,25 +3,37 @@ package com.bridgelabz.genricprogram;
 public class GenericProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program");
-        compare(4,2,6);
-        System.out.println("Float Max Number: " + compare(1.2f,4.2f,6.8f));
-        System.out.println("String Max: "+compare("interger","float","string"));
+        intergerMax(4,2,6);
+        floatMax(1.2f,4.5f,6.8f);
     }
     //calling integerMax static method and passing the parameter
 
     /*
-     * generic type method to compare any type of data
+     * method used to find maximum of three inetergr number
      * @param a, b ,c and compareTo method to find the max number
      */
-    public static <E extends Comparable<E>> E compare(E a, E b, E c){
-        E max = a;
+    public static void intergerMax(Integer a,Integer b,Integer c){
+        int max = a;
         if(b.compareTo(max)>0){
             max = b;
         }
         if(c.compareTo(max)>0){
             max = c;
         }
-        return max;
-
+        System.out.print("Integer Max: "+max);
+    }
+    /*
+     * floatMax method compraeTo maximum float number
+     * floatMax is given as parametrise method
+     */
+    public static void floatMax(Float a, Float b, Float c){
+        Float max = a;
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.print("flot max"+max);
     }
 }
