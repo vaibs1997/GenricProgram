907cdef579ba68db0c101ee548ba76f021e66c52
vaibs <vaibhavmuratkar2@gmail.com>
2022-01-06 00:15:31 +0530

    Adding the method to compare string

--- src/com/bridgelabz/genricprogram/GenericProgram.java
+++ src/com/bridgelabz/genricprogram/GenericProgram.java
@@ -1,39 +1,54 @@
 package com.bridgelabz.genricprogram;
 
-public class GenericProgram <E extends Comparable<E>>{
-    E a,b,c;
-    /*
-     * constructor point to the a, b ,c
-     * constructor take paramter any type of data
-     */
-    public GenericProgram(E a, E b, E c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
+public class GenericProgram {
     public static void main(String[] args) {
         System.out.println("welcome to generic program");
-        compare(4,2,6);
-        System.out.println("float max number" + compare(1.2f,4.2f,6.8f));
-        System.out.println("string max"+compare("interger","float","string"));
+        intergerMax(4,2,6);
+        floatMax(1.2f,4.5f,6.8f);
+        stringMax("interger","float","string");
     }
     //calling integerMax static method and passing the parameter
 
     /*
-     * generic type method to compare any type of data
+     * method used to find maximum of three inetergr number
      * @param a, b ,c and compareTo method to find the max number
      */
-    public static <E extends Comparable<E>> E compare(E a, E b, E c){
-        E max = a;
+    public static void intergerMax(Integer a,Integer b,Integer c){
+        int max = a;
         if(b.compareTo(max)>0){
             max = b;
         }
         if(c.compareTo(max)>0){
             max = c;
         }
-        return max;
-
+        System.out.print("integer max"+max);
     }
-
+    /*
+     * floatMax method compraeTo maximum float number
+     * floatMax is given as parametrise method
+     */
+    public static void floatMax(Float a, Float b, Float c){
+        Float max = a;
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("flot max"+max);
+    }
+    /*
+     * StringMax method to compute max string
+     * compareto method use to compare the three string
+     */
+    public static void stringMax(String a, String b, String c){
+        String max = a;
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("string max"+max);
+    }
 }
